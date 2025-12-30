48020ed3fd38c2d4775bd7aa59b504f7aec71b1b
shravan <shravangulekar181206@gmail.com>
2025-12-30 18:55:06 +0530

    sum of 2

---
+++ sum-of-2-num/script.js
@@ -0,0 +1,8 @@
+function addNumber(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    let sum = a+b;
+
+    document.getElementById("result").innerText = "Result:" + sum;
+}
